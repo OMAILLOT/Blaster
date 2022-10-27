79b834f064582c965e71b51488e86f14d7a9403c
ChronoBlasted <74500014+ChronoBlasted@users.noreply.github.com>
2022-10-27 22:58:33 +0200

    feat : transition to equipement menu and come back to menu

--- Assets/Scripts/UI/MenuCanvas.cs
+++ Assets/Scripts/UI/MenuCanvas.cs
@@ -1,11 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+public enum MenuState { WAIT, MENU, EQUIPEMENT }
 
 public class MenuCanvas : MonoBehaviour
 {
+
+    MenuState state;
+
+    [SerializeField] RectTransform menu,equipement;
+
     public void Init()
     {
+        state = MenuState.MENU;
+    }
 
+    public void OpenEquipement()
+    {
+        if (state == MenuState.WAIT) return;
+
+        state = MenuState.WAIT;
+
+        menu.DOAnchorMin(new Vector2(0, 1), .3f);
+        menu.DOAnchorMax(new Vector2(1, 2), .3f).OnComplete(() => state = MenuState.EQUIPEMENT);
+
+        equipement.DOAnchorMin(new Vector2(0, 0), .3f);
+        equipement.DOAnchorMax(new Vector2(1, 1), .3f).OnComplete(() => state = MenuState.EQUIPEMENT);
+    }
+
+    public void CloseEquipement()
+    {
+        if (state == MenuState.WAIT) return;
+
+        state = MenuState.WAIT;
+
+        menu.DOAnchorMin(new Vector2(0, 0), .3f);
+        menu.DOAnchorMax(new Vector2(1, 1), .3f).OnComplete(() => state = MenuState.MENU);
+
+        equipement.DOAnchorMin(new Vector2(0, -1), .3f);
+        equipement.DOAnchorMax(new Vector2(1, 0), .3f).OnComplete(() => state = MenuState.MENU);
     }
 }
