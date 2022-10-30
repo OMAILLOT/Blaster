623af150b4044739e3b2072f230d799fa4ae5371
ChronoBlasted <74500014+ChronoBlasted@users.noreply.github.com>
2022-10-30 17:22:40 +0100

    Implementing Settings UI

--- Assets/Scripts/UI/MenuCanvas.cs
+++ Assets/Scripts/UI/MenuCanvas.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-public enum MenuState { WAIT, MENU, EQUIPEMENT }
+public enum MenuState { WAIT, MENU, EQUIPEMENT, SETTINGS }
 
 public class MenuCanvas : MonoBehaviour
 {
 
     MenuState state;
 
-    [SerializeField] RectTransform menu, equipement;
+    [SerializeField] RectTransform menu, equipement, settings;
 
     [SerializeField] EquipementScreen equipementScreen;
 
@@ -44,5 +44,31 @@ public class MenuCanvas : MonoBehaviour
 
         equipement.DOAnchorMin(new Vector2(0, -1), .3f);
         equipement.DOAnchorMax(new Vector2(1, 0), .3f).OnComplete(() => state = MenuState.MENU);
+    }
+
+    public void OpenSettings()
+    {
+        if (state == MenuState.WAIT) return;
+
+        state = MenuState.WAIT;
+
+        menu.DOAnchorMin(new Vector2(0, -1), .3f);
+        menu.DOAnchorMax(new Vector2(1, 0), .3f).OnComplete(() => state = MenuState.SETTINGS);
+
+        settings.DOAnchorMin(new Vector2(0, 0), .3f);
+        settings.DOAnchorMax(new Vector2(1, 1), .3f).OnComplete(() => state = MenuState.SETTINGS);
+    }
+
+    public void CloseSettings()
+    {
+        if (state == MenuState.WAIT) return;
+
+        state = MenuState.WAIT;
+
+        menu.DOAnchorMin(new Vector2(0, 0), .3f);
+        menu.DOAnchorMax(new Vector2(1, 1), .3f).OnComplete(() => state = MenuState.MENU);
+
+        settings.DOAnchorMin(new Vector2(0, 1), .3f);
+        settings.DOAnchorMax(new Vector2(1, 2), .3f).OnComplete(() => state = MenuState.MENU);
     }
 }
